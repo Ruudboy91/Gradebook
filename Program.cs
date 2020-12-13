403243193561e846a7fdeedf2e989e67d4ee1ab4
Rudi Andre <rudi.andre@hotmail.com>
2020-12-13 14:34:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Scott's Grade Book");
+            book.Addgrade(89.1);
+            book.Addgrade(90.5);
+            book.Addgrade(77.5);
+            book.ShowStatistics();
+
+
+        }
+    }
+}
